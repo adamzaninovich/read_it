b764bce5118a4fb22d75882a5f410d7f00b7fecd
Adam Zaninovich <adam.zaninovich@activenetwork.com>
2014-08-23 15:32:11 -0700

    ties it all together

--- lib/read_it.rb
+++ lib/read_it.rb
@@ -1,5 +1,21 @@
 require "read_it/version"
+require "read_it/configurable"
+require "read_it/http"
+require "read_it/sub"
+require "read_it/post"
 
 module ReadIt
-  # Your code goes here...
+  extend ReadIt::Configurable
+  extend ReadIt::HTTP
+  self.adapter = Faraday.default_adapter
+
+  def self.sub name
+    Sub.new name
+  end
 end
+
+# Configuration Example
+#
+# ReadIt.configure do |config|
+#   config.adapter = <faraday adapter>
+# end
